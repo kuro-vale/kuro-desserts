1a60a256131eff201b63ff0721c53d04e5735f34
kuro-vale <jsalcedo218@gmail.com>
2022-08-17 17:51:38 -0500

    Add flavor model

---
+++ Controllers/Flavor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kuro_desserts.Controllers;
+
+/// <summary>
+/// Change the dessert flavor, yummy
+/// </summary>
+public class Flavor
+{
+    [Key] public Guid Id { get; set; }
+
+    /// <summary>
+    /// The name of the flavor
+    /// </summary>
+    /// <example>Salted Caramel</example>
+    [Required, MinLength(3, ErrorMessage = "Please use a name bigger than 3 characters"),
+     MaxLength(50, ErrorMessage = "Please use a name less than 50 characters")]
+    public string? Name { get; set; }
+}
